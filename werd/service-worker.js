d3d8977b4bc9b30435988b7c4b23310e1090b528
ghalllay-ux <ghalllay@gmail.com>
2026-09-02 08:21:42 +0300

    Cache Werd More hub

--- werd/service-worker.js
+++ werd/service-worker.js
@@ -1,6 +1,6 @@
-const APP_CACHE='werd-v8';
-const RUNTIME_CACHE='werd-runtime-v8';
-const APP_SHELL=['./','./index.html','./app.css','./app.js','./notifications.js','./features.js','./reader.js','./listening.js','./prayer.js','./manifest.webmanifest','./icon.svg'];
+const APP_CACHE='werd-v9';
+const RUNTIME_CACHE='werd-runtime-v9';
+const APP_SHELL=['./','./index.html','./app.css','./app.js','./notifications.js','./features.js','./reader.js','./listening.js','./prayer.js','./more.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{event.waitUntil(caches.open(APP_CACHE).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>![APP_CACHE,RUNTIME_CACHE].includes(k)).map(k=>caches.delete(k)))));self.clients.claim();});
