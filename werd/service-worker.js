03908bb8ee1dd0191682a2c49a815255167a96cd
ghalllay-ux <ghalllay@gmail.com>
2026-09-02 06:34:26 +0300

    Cache advanced Quran reader

--- werd/service-worker.js
+++ werd/service-worker.js
@@ -1,6 +1,6 @@
-const APP_CACHE='werd-v4';
-const RUNTIME_CACHE='werd-runtime-v4';
-const APP_SHELL=['./','./index.html','./app.css','./app.js','./notifications.js','./features.js','./manifest.webmanifest','./icon.svg'];
+const APP_CACHE='werd-v5';
+const RUNTIME_CACHE='werd-runtime-v5';
+const APP_SHELL=['./','./index.html','./app.css','./app.js','./notifications.js','./features.js','./reader.js','./manifest.webmanifest','./icon.svg'];
 
 self.addEventListener('install',event=>{event.waitUntil(caches.open(APP_CACHE).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>![APP_CACHE,RUNTIME_CACHE].includes(k)).map(k=>caches.delete(k)))));self.clients.claim();});
